e779a5ed55b4a184bba14124bd18426dec7e8835
NyangoyaG <nyangoyagb@gmail.com>
2022-12-14 15:46:43 -0800

    Printing every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,22 +1,26 @@
 #include "main.h"
 
 /**
- * jack_bauer - printing every minute of the day
- * Return: starting from 00:00 to 23:59
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: Always 0.
  */
-void jack_bauer(void)
-{
-	int p, q;
 
-	for (p = 0; p <= 23; p++)
+void jack_bauer(void)
+
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 23; a++)
 	{
-		for (q = 0; q <= 59; q++)
+		for (b = 0; b <= 59; b++)
 		{
-			_putchar (p / 10 + '0');
-			_putchar (p % 10 + '0');
+			_putchar (a / 10 + '0');
+			_putchar (a % 10 + '0');
 			_putchar (':');
-			_putchar ((q / 10) + '0');
-			_putchar ((q % 10) + '0'); 
+			_putchar (b / 10 + '0');
+			_putchar (b % 10 + '0');
 			_putchar ('\n');
 		}
 	}
