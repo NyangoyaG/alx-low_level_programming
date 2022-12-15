8f18fd3d28632c4d27ea94c81a880939b791ab6e
NyangoyaG <nyangoyagb@gmail.com>
2022-12-15 14:21:17 -0800

    function that checks for a digit (0 through 9)

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * int mul(int a, int b) - multiplication
+ * 
+ * @a first integer
+ *
+ * @b secong integer
+ *
+ * Return: product of the two multiplied integers
+ */
+int mul(int a, int b);
+{
+	int product;
+	product = a * b;
+	return (product)
+}
