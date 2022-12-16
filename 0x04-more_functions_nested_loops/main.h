b3b5152ee03dfb2544b88aab5bd5acb638e873de
NyangoyaG <nyangoyagb@gmail.com>
2022-12-15 16:55:09 -0800

    function that prints a triangle, followed by a new line

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,6 +4,7 @@ int _putchar(char c);
 int mul(int a, int b);
 int _isupper(int c);
 int _isdigit(int c);
+void print_triangle(int size);
 void more_numbers(void);
 void print_numbers(void);
 void print_most_numbers(void);
