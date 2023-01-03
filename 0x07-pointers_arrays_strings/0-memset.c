7bbc4628b724414b039e538d2a0a930ba39ad1c9
NyangoyaG <nyangoyagb@gmail.com>
2023-01-03 07:27:14 -0800

    Commit new year

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,17 +4,15 @@
 /**
  *_memset - prints buffer in hexa
  * @s: A pointer to the memory area to be filled.
- * @c: The character to fill the memory area with.
+ * @b: The character to fill the memory area with.
  * @n: The number of bytes to be filled.
  * * Return: Nothing.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-	unsigned char *memory = s, value = c;
+	unsigned int index;
 
-	for (index = 0; i < n; i++)
-		imemory[i] = value;
-
-	return (memory);
+	for (index = 0; index < n; index++)
+		*(s + index) = b;
+	return (s);
 }
