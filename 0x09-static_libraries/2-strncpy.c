2c15a5ec432ff3d5b20b06601740024c5c65cc24
NyangoyaG <nyangoyagb@gmail.com>
2023-01-11 14:33:00 +0300

    strncpy

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strncpy - prototype
+ * @dest: parameter
+ * @src: parameter
+ * @n: parameter
+ * Return: 0 or 1
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	Return 0;
+}
