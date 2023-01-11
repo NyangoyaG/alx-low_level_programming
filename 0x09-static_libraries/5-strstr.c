be280e6dbc759e1805745307cd3110ac676ac9e6
NyangoyaG <nyangoyagb@gmail.com>
2023-01-11 15:45:00 +0300

    strstr

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strstr - prototype
+ * @haystack: parameter
+ * @needle: parameter
+ * Return: 0 and 1
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	Return 0;
+}
