c9d29cdc8917da3eff6affa684015ed0ea9950c7
NyangoyaG <nyangoyagb@gmail.com>
2023-01-11 13:03:17 +0300

    memcpy

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,13 @@
+#include "mai.h"
+
+/**
+ * _memcpy - prototype
+ * @dest: paraameter
+ * @src: parameter
+ * @n: parameter
+ * Return: 0 or 1
+ */
+char *_memcpy(char *dest, char *src, insigned int n)
+{
+	Return 0;
+}
