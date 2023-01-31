dd605a4545278feaf44dd4276e393aa29fc53909
NyangoyaG <nyangoyagb@gmail.com>
2023-01-30 22:28:52 -0800

    lists file updated

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -8,13 +8,13 @@
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
- * @next: points to the next node     
+ * @next: points to the next node
  */
 typedef struct list_s
 {
 	    char *str;
-	        unsigned int len;
-		    struct list_s *next;
+	    unsigned int len;
+	    struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
